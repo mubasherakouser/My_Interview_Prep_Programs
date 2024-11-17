74bcf380813fd53942fb5e472580eb752967fd5b
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2024-11-17 10:42:46 +0530

    Leap_Year.java

---
+++ Leap_Year.java
@@ -0,0 +1,12 @@
+//Leap year program in java
+class Example{
+    public static void main(String[] args){
+        int year = 2020;
+
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            System.out.println(year + " is leap year");
+        } else {
+            System.out.println(year + " is not leap year");
+        }
+    }
+}
