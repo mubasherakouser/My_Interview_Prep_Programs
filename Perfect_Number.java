5fe86fc9fed513ce195a1e73e70eb4ed8ec723a3
Banaala Mubashera Kouser <149088796+mubasherakouser@users.noreply.github.com>
2024-11-17 12:40:53 +0530

    Perfect_Number.java

--- Perfect_Number.java
+++ Perfect_Number.java
@@ -1,4 +1,11 @@
-//Perfect Number java code
+/*Perfect Number
+A perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself.
+Example:
+Consider the number 6:
+ * The divisors of 6 are: 1, 2, 3, and 6.
+ * The proper divisors of 6 are: 1, 2, and 3.
+ * The sum of the proper divisors is: 1 + 2 + 3 = 6.
+Since the sum of the proper divisors is equal to the number itself, 6 is a perfect number.*/
 import java.util.Scanner;
 class Demo {
     public static void main(String[] args) {
